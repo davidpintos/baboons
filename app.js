e878b18997dd0559aa1132361dec954a6868f281
David <davidpintos@gmail.com>
2012-10-31 17:39:24 -0300

    new folders

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var http = require("http");
-http.createServer(function (request,response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hola Mundo");
-  response.end();
-}).listen(8888);
+var express = require('express');
+var app = express.createServer();
+var io = require('socket.io').listen(app);
+app.listen(8080, function() {
+        console.log("--- app.listen");
+});
