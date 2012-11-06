055c230a0c3ae05e64c7ff3a63fe3ac22576ffa3
David <davidpintos@gmail.com>
2012-11-06 17:42:46 -0300

    express added, app config modified.

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
 
     // home page
     app.get('/', function(req, res) {        
-        res.render('index', { title: 'Champagne' })
+        res.render('index', { title: 'Baboon' })
     });
 
 }
