c00e6e9d09d8dd6babe0bf1cea49b44b6c2b12cb
David <davidpintos@users.noreply.github.com>
2022-02-03 19:40:43 -0300

    Create velosimo2.js

---
+++ velosimo2.js
@@ -0,0 +1,35 @@
+async function postData(url = '', data = {}) {
+
+  const response = fetch(url, {
+    method: 'POST',
+    headers: new Headers({
+       'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify(data)
+  }).then(response => response.json());
+  
+  return response;
+}
+
+
+$( document ).ready(function() {
+  $("#form1").append('<input type="button" class="action-btn checkRequired Submit Submitnuevo" name="action" aria-labelledby="action" value="Send" aria-invalid="false">');
+  
+  $("#form1 input[type='submit']").hide();
+  
+  $("#form1 .Submitnuevo").click(async( event ) => {
+    const days = $("#q1 input").val();
+	const amount = $(".velosimo-total input").val();
+    
+  	if (days.trim() === '' || days.trim() ==='0') {
+    	alert('Days are required');
+    	$("#q1 input").focus();
+		return false;
+  	}
+      
+	window.location.replace("https://dev.velosimo.io/app/payment_adapter/laserfiche/forte/QTQxMjYwNDkzMzo6dkNNeXhfcmVKd0pqcDJrXzRYQ3M=/initialize_payment?amount="+amount);
+
+    event.preventDefault();
+    event.stopPropagation();
+  });
+});
